b42625ba631085ddb184fa1265e5d81462e1f1e4
Xunzhou Ye <yex33@mcmaster.ca>
2025-04-16 14:10:57 -0400

    add unit tests for concepts

--- include/fp_concepts.hpp
+++ include/fp_concepts.hpp
@@ -16,6 +16,11 @@ constexpr bool is_all_floating_point_v<T> = std::floating_point<T>;
 template <typename... Ts>
 concept FloatingPoint = is_all_floating_point_v<Ts...>;
 
+static_assert(FloatingPoint<double>);
+static_assert(FloatingPoint<double, double>);
+static_assert(!FloatingPoint<int>);
+static_assert(!FloatingPoint<double, int>);
+
 template <typename T, typename... Ts>
 constexpr bool is_partial_ordered_v = ((std::numeric_limits<T>().epsilon() >=
                                         std::numeric_limits<Ts>().epsilon()) &&
@@ -28,8 +33,28 @@ constexpr bool is_partial_ordered_v<T> = true;
 template <typename... Ts>
 concept PartialOrdered = is_partial_ordered_v<Ts...>;
 
+static_assert(PartialOrdered<double>);
+static_assert(PartialOrdered<double, double, double>);
+static_assert(PartialOrdered<float, double, double>);
+static_assert(PartialOrdered<float, float, double>);
+static_assert(!PartialOrdered<double, double, float>);
+static_assert(!PartialOrdered<double, float, double>);
+static_assert(!PartialOrdered<double, float, float>);
+static_assert(!PartialOrdered<double, double, float>);
+
 template <typename... Ts>
 concept Refinable = FloatingPoint<Ts...> && PartialOrdered<Ts...>;
+
+static_assert(Refinable<double>);
+static_assert(Refinable<double, double, double>);
+static_assert(Refinable<float, double, double>);
+static_assert(Refinable<float, float, double>);
+static_assert(!Refinable<double, double, float>);
+static_assert(!Refinable<double, float, double>);
+static_assert(!Refinable<double, float, float>);
+static_assert(!Refinable<double, double, float>);
+static_assert(!Refinable<int>);
+static_assert(!Refinable<double, int>);
 
 // End of FP_CONCEPTS_HPP
 #endif
