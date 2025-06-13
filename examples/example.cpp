a07071dd78970036ce8550859e79e1d663354aad
Xunzhou Ye <yex33@mcmaster.ca>
2025-06-13 17:35:04 -0400

    update example

--- examples/example.cpp
+++ examples/example.cpp
@@ -26,7 +26,8 @@ int main(int argc, char* argv[]) {
   }
   const fs::path mtx_path{argv[1]};
   if (!fs::exists(mtx_path)) {
-    std::cout << "Error: Matrix file does not exist at " << mtx_path << std::endl;
+    std::cout << "Error: Matrix file does not exist at " << mtx_path
+              << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -44,7 +45,7 @@ int main(int argc, char* argv[]) {
     options.generalize_symmetry = false;
     std::ifstream            fin{mtx_path};
     std::vector<std::size_t> rs, cs;
-    std::vector<double>          vs;
+    std::vector<double>      vs;
     // rs and cs are swapped here, matrix read is transposed
     fmm::read_matrix_market_triplet(fin, header, cs, rs, vs, options);
     std::cout << (header.symmetry == fmm::symmetry_type::symmetric
@@ -72,8 +73,9 @@ int main(int argc, char* argv[]) {
                            [&](const std::size_t i) { return rs[i]; });
     std::ranges::transform(perm, std::back_inserter(cols),
                            [&](const std::size_t i) { return cs[i]; });
-    std::ranges::transform(perm, std::back_inserter(vals),
-                   [&](std::size_t i) { return static_cast<UW>(vs[i]); });
+    std::ranges::transform(perm, std::back_inserter(vals), [&](std::size_t i) {
+      return static_cast<UW>(vs[i]);
+    });
   }
 
   auto                     n   = static_cast<std::size_t>(header.nrows);
@@ -92,8 +94,24 @@ int main(int argc, char* argv[]) {
   }
 
   GmresLDLIR<UF, UW, UR> solver;
-  solver.SetTolerance(1e-40);
-  solver.Compute(std::move(Ap), std::move(Ai), std::move(Ax));
-  std::vector<UW> b(n, 1);
-  solver.Solve(b);
+  solver.SetTolerance(1e-16);
+  solver.SetMaxIRIterations(5);
+  solver.Compute(Ap, Ai, Ax);
+  std::vector<UW> x = solver.Solve(std::vector<UW>(n, 1));
+
+  GmresLDLIR<std::float64_t, dd_real, qd_real> solver_ref;
+  solver_ref.SetTolerance(1e-40);
+  solver_ref.SetMaxIRIterations(3);
+  solver_ref.Compute(Ap, Ai, Ax);
+  std::vector<dd_real> xref = solver_ref.Solve(std::vector<dd_real>(n, 1));
+
+  std::cout << "result: "
+            << InfNrm(VectorSubtract<dd_real>(x, xref)) / InfNrm(xref)
+            << std::endl;
+
+  // std::vector<UW> xref(n, 1);
+  // std::vector<UW> b = MatrixMultiply<UW, UR>(Ap, Ai, Ax, xref);
+  // std::vector<UW> x = solver.Solve(b);
+  // std::cout << "result: " << InfNrm(VectorSubtract<double>(x, xref)) <<
+  // std::endl;
 }
